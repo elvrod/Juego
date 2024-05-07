2adf539215cc10c84c08987cf9c5c1a5016495e7
Elvis Rodriguez <elvis.rodriguezsantos@educa.madrid,org>
2024-05-07 17:06:07 +0200

    version esqueleto

---
+++ Juegos/src/juegos/principal/Principal.java
@@ -0,0 +1,12 @@
+package juegos.principal;
+
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bienvenido pe causa");
+	}
+
+}
